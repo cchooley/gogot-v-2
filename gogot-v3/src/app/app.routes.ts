2684e9476aa23ba9cbd916be92eeae434c5ebb34
Conor Hooley <cchooley@gmail.com>
2018-09-18 23:42:29 -0600

    Auth with email/pw; fb & google implemented and functional

--- gogot-v3/src/app/app.routes.ts
+++ gogot-v3/src/app/app.routes.ts
@@ -4,9 +4,12 @@ import { LoginComponent } from './components/login/login.component';
 import { UserComponent } from './components/user/user.component';
 import { RegisterComponent } from './components/register/register.component';
 
+import { UserResolver } from './components/user/user.resolver';
+import { AuthGuard } from './services/auth.guard';
+
 export const rootRouterConfig: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent },
-    { path: 'user', component: UserComponent }
+    { path: 'login', component: LoginComponent, canActivate: [AuthGuard] },
+    { path: 'register', component: RegisterComponent, canActivate: [AuthGuard] },
+    { path: 'user', component: UserComponent, resolve: { data: UserResolver } }
 ];
--- gogot-v3/src/app/services/user.service.ts
+++ gogot-v3/src/app/services/user.service.ts
@@ -1,6 +1,4 @@
-
 import { Injectable } from "@angular/core";
-import 'rxjs/add/operator/toPromise';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
@@ -33,8 +31,8 @@ export class UserService {
       user.updateProfile({
         displayName: value.name,
         photoURL: user.photoURL
-      }).then(res => {
-        resolve(res)
+      }).then(() => {
+        resolve()
       }, err => reject(err))
     })
   }
